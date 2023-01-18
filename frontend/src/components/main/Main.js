e46e9725ccc5206571de91f2d89594ca8461ea18
leeseunghan <seunghan4769@naver.com>
2023-01-18 14:34:13 +0900

    create-pjt-frontend

---
+++ frontend/src/components/main/Main.js
@@ -0,0 +1,11 @@
+import classes from "./Main.module.css";
+
+const Main = () => {
+  return (
+    <div className={classes.main}>
+      <h1>메인 페이지</h1>
+    </div>
+  );
+};
+
+export default Main;

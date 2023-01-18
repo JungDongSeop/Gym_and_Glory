5f6160e1d2eb8b3e77b155e955389fd8cf263a8d
leeseunghan <seunghan4769@naver.com>
2023-01-18 17:29:46 +0900

    components

--- frontend/src/components/main/Main.js
+++ frontend/src/components/main/Main.js
@@ -1,6 +1,10 @@
 import classes from "./Main.module.css";
 
 const Main = () => {
+  const ButtonToggleHandler = () => {
+    console.log("hello");
+  };
+
   return (
     <div className={classes.main}>
       <h1>메인 페이지</h1>

aa5ffdf8736b8a7eeebeba579325dfb19660c661
JungDongSeop <ds123911@gmail.com>
2023-01-20 11:44:37 +0900

    ADD: [front] 메인페이지 캐러셀 추가

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,7 +5,6 @@ import Signup from "./components/auth/signup/Signup";
 import FindPwd from "./components/auth/find_pwd/FindPwd";
 import Lobby from "./components/lobby/Lobby";
 import MyPage from "./components/mypage/MyPage";
-import NoticeBoard from "./components/board/NoticeBoard";
 
 import "./App.css";
 
@@ -20,7 +19,6 @@ function App() {
         <Route path="/find-pwd" element={<FindPwd />} />
         <Route path="/lobby" element={<Lobby />} />
         <Route path="/mypage/:username" element={<MyPage />} />
-        <Route path="/board/notice" element={<NoticeBoard />} />
       </Routes>
     </div>
   );
--- frontend/src/components/main/Main.js
+++ frontend/src/components/main/Main.js
@@ -1,17 +1,55 @@
+import React from 'react';
 import classes from "./Main.module.css";
 import { useNavigate } from "react-router-dom";
-// import { Link, useNavigate } from "react-router-dom";
+import { Carousel } from 'antd';
+import Logo from "../../assets/logo.png";
 
 const Main = () => {
   // custom react hook
   const navigate = useNavigate();
+
+  // 캐러셀 페이지 넘기기
+  const carouselRef = React.createRef();
 
 
   return (
     <div className={classes.main}>
       {/* 로그인하지 않은 유저에게 표시되는 화면 */}
       <h1>메인 페이지. 제일 처음 보이는 화면입니다.</h1>
-      <button onClick={() => navigate("/login")}>로그인 버튼</button>
+
+      {/* 캐러셀 넣기 */}
+      <Carousel className={classes.carousel} ref={carouselRef}>
+        <div>
+          <span className={classes.carouselBox}>
+            <img src={Logo} alt={Logo}></img>
+            <p>푸쉬업, 플랭크, 런지, 버피 Let`s Go</p>
+            <button onClick={() => navigate("/login")}>로그인 버튼</button>
+            <br/>
+            <button onClick={() => {carouselRef.current.prev()}}>스토리</button>
+            <button onClick={() => {carouselRef.current.next()}}>가이드</button>
+          </span>
+        </div>
+        <div>
+          <span className={classes.carouselBox}>
+            <h2>스토리</h2>
+            <p>옛날 옛적에 ...</p>
+            <br/>
+            <button onClick={() => {carouselRef.current.prev()}}>가이드</button>
+            <button onClick={() => {carouselRef.current.next()}}>시작</button>
+          </span>
+        </div>
+        <div>
+          <span className={classes.carouselBox}>
+            <h2>가이드</h2>
+            <p>이래 저래 하세요</p>
+            <br/>
+            <button onClick={() => {carouselRef.current.prev()}}>시작</button>
+            <button onClick={() => {carouselRef.current.next()}}>스토리</button>
+          </span>
+        </div>
+
+      </Carousel>
+      
       <button onClick={() => navigate("/signup")}>회원가입 버튼</button>
       <button onClick={() => navigate("/find-pwd")}>비밀번호 찾기 버튼</button>
 

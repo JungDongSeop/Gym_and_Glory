adaf9d7c137a82efbf176fc44bf364ee6b2d84bc
LJH <pliot123@naver.com>
2023-01-30 11:01:19 +0900

    FIX : [back] 좋아요 로직 수정

--- backend/src/main/java/com/backend/db/entity/BoardGood.java
+++ backend/src/main/java/com/backend/db/entity/BoardGood.java
@@ -5,10 +5,7 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Data
@@ -19,6 +16,10 @@ import javax.persistence.Table;
 public class BoardGood {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "board_good_sequence")
+    private Integer boardGoodSequence;
+
     @Column(name = "user_sequence")
     private Integer userSequence;
 

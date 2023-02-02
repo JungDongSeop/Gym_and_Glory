af2cde78d711fdf10e3d49cbf3eca44b2a7326bf
SuminSon <ssm0224@gmail.com>
2023-02-02 13:43:19 +0900

    STYLE: [front] 컴포넌트 스타일 작업(버튼, 메인화면, 스토리화면)

--- frontend/src/components/main/Main.js
+++ frontend/src/components/main/Main.js
@@ -1,9 +1,10 @@
 import React, { useContext } from "react";
-import classes from "./Main.module.css";
+import classes from "./Main.module.scss";
 import { useNavigate } from "react-router-dom";
 import { Carousel } from "antd";
 import AuthContext from "../../store/auth-context";
 import Logo from "../../assets/logo.svg";
+import story_img1 from "../../assets/story_img1.PNG";
 import Button from "../../components/UI/Button";
 
 const Main = () => {
@@ -24,8 +25,10 @@ const Main = () => {
         <div>
           <span className={classes.carouselBox}>
             <img src={Logo} alt={Logo}></img>
-            <h1>푸쉬업, 플랭크, 런지, 버피 </h1>
+            <div className={classes.mainTextDiv}>
+              <h1>푸쉬업, 플랭크, 런지, 버피 </h1>
               <h1>Let`s Go</h1>
+            </div>
             {!isLoggedIn && (
               <div>
                 <Button onClick={() => navigate("/login")}>로그인</Button>
@@ -59,8 +62,20 @@ const Main = () => {
         <div>
           <span className={classes.carouselBox}>
             <img className={classes.smallLogo} src={Logo} alt={Logo}></img>
-            <h2>스토리</h2>
-            <p>옛날 옛적에 ...</p>
+            <div className={classes.storyDiv}>
+              <h2>스토리</h2>
+              <img src={story_img1} alt={story_img1}></img>
+              <p>
+                여러분은 이 땅에서 가장 위대한 드래곤 슬레이어가 되는 꿈을 꾸며 여행을 떠나는 젊은 강아지들입니다.
+                (때때로 당신은 사람일 수도 있습니다.)
+                이 목표를 달성하기 위해서는 먼저 신체적으로 강하고 전투에 능숙해야 합니다.
+                이를 위해서는 스쿼트, 팔 굽혀 펴기 및 기타 운동과 같은 일상적인 근육 훈련에 참여해야 합니다.
+                당신은 여행을 떠나며 점점 더 강하고 다양한 드래곤과 만나 전투를 벌이게 됩니다.
+                힘과 기술이 성장함에 따라 당신은 결국 가장 무시무시한, 전설 속 붉은 숲의 고대 드래곤과의 마지막 사투에 직면하게 될 것입니다.
+                만약 당신이 그 드래곤을 무찌른다면 드래곤 머슬 마스터라는 칭호를 얻고 지상 최고의 드래곤 슬레이어가 됩니다.
+                그 이름은 영원토록 (DB가 허용하는 기간동안) 후세에 전해질 것입니다.
+            </p>
+            </div>
             <br />
             <span
               className={classes.leftword}

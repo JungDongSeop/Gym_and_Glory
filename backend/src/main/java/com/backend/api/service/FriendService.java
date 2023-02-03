d05666ae6cfbbae85b3b316f9c4c1107f3db1a6f
LJH <pliot123@naver.com>
2023-02-03 15:21:41 +0900

    ADD :[back] 사진추가, 친구 삭제 API

--- backend/src/main/java/com/backend/api/service/FriendService.java
+++ backend/src/main/java/com/backend/api/service/FriendService.java
@@ -7,6 +7,7 @@ import com.backend.db.repository.SendFriendRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
 @Service
@@ -42,5 +43,11 @@ public class FriendService {
 
     public List<Friend> getList(Integer userSequence) {
         return friendRepository.findByGetSequence(userSequence);
+    }
+
+    @Transactional
+    public void deleteFriend(Integer getSequence, Integer sendSequence) {
+        friendRepository.deleteByGetSequenceAndSendSequence(getSequence,sendSequence);
+        friendRepository.deleteByGetSequenceAndSendSequence(sendSequence,getSequence);
     }
 }

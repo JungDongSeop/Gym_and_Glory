4d64ef20af2c65e1af85d2a68e60973eaba30e4f
binsid1 <binsid1@naver.com>
2023-02-07 16:50:47 +0900

    STYLE: [front] 게임방 나가기 로직 수정

--- frontend/src/components/lobby/room_list/RoomList.js
+++ frontend/src/components/lobby/room_list/RoomList.js
@@ -19,40 +19,47 @@ class RoomList extends Component {
   }
 
   componentDidMount() {
-    // 현재는 오픈비두 서버에 직접 요청을 보내 열려있는 방 정보를 들고 오지만
-    // 추후 백에 요청을 보내어 현재 열려있는 방의 정보를 받아오기
-    // 오픈비두에 직접 요청을 보내니 내가 커스텀한 방제와 팀명이 오지 않음
-    // axios.get("https://i8e107.p.ssafy.io:8443/openvidu/api/sessions",)
     axios
-      .get(`${RestApi()}/lobby`, {
+      .get("https://i8e107.p.ssafy.io:8443/openvidu/api/sessions", {
         headers: {
           Authorization: `Basic ${btoa(`OPENVIDUAPP:MY_SECRET`)}`,
         },
       })
       .then((response) => {
-        console.log(response.data.content);
-        const info = response.data.content.map((room) => {
-          const title = room.title;
-          const teamName = room.teamName;
-          const peopleNum = room.count;
-          const isopened = !room.privateStatus;
-          const roomPassword = room.password;
-          const roomId = room.sessionKey;
-          const roomStatus = room.roomStatus;
-
-          return {
-            title,
-            teamName,
-            peopleNum,
-            isopened,
-            roomPassword,
-            roomId,
-            roomStatus,
-          };
-        });
-        console.log(info);
-        this.setState({ rooms: info });
+        console.log(response);
       });
+    setInterval(() => {
+      axios
+        .get(`${RestApi()}/lobby`, {
+          headers: {
+            Authorization: `Basic ${btoa(`OPENVIDUAPP:MY_SECRET`)}`,
+          },
+        })
+        .then((response) => {
+          console.log(response.data.content);
+          const info = response.data.content.map((room) => {
+            const title = room.title;
+            const teamName = room.teamName;
+            const peopleNum = room.count;
+            const isopened = !room.privateStatus;
+            const roomPassword = room.password;
+            const roomId = room.sessionKey;
+            const roomStatus = room.roomStatus;
+
+            return {
+              title,
+              teamName,
+              peopleNum,
+              isopened,
+              roomPassword,
+              roomId,
+              roomStatus,
+            };
+          });
+          console.log(info);
+          this.setState({ rooms: info });
+        });
+    }, 1000);
   }
 
   render() {

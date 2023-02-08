0312e06b0892d46461f431be47ffe4826374543d
JungDongSeop <ds123911@gmail.com>
2023-02-08 13:38:39 +0900

    FIX: [front] 친구 추가 수정 중

--- frontend/src/components/layout/side_bar/FriendListModalAdd.js
+++ frontend/src/components/layout/side_bar/FriendListModalAdd.js
@@ -1,4 +1,4 @@
-import { useState, useRef } from "react";
+import { useState, useRef, useEffect } from "react";
 import { useContext } from "react";
 import AuthContext from "../../../store/auth-context";
 import axios from "axios";
@@ -6,7 +6,7 @@ import RestApi from "../../api/RestApi";
 
 const FriendListModalAdd = (props) => {
   // redux로 user 정보 가져오기
-  const { userSequence } = useContext(AuthContext);
+  const { userSequence, nickname } = useContext(AuthContext);
 
   // 닉네임으로 유저 검색
   // 닉네임 저장할 변수
@@ -19,7 +19,7 @@ const FriendListModalAdd = (props) => {
     // 닉네임이 있으면 해당 닉네임으로 axios 요청
     if (reportUserName.trim()) {
       await axios
-        .get(`${RestApi()}/search/nickname/${reportUserName}`)
+        .get(`http://localhost:8080/api/search?${userSequence}&nickName?${reportUserName}`)
         .then((res) => {
           setSearchedDatas(res.data);
         })
@@ -28,50 +28,56 @@ const FriendListModalAdd = (props) => {
   };
 
   // 친구 요청 보내기
-  const handleSendFriendRequest = async (getter) => {
+  const handleSendFriendRequest = async (getterSequence) => {
     alert("친구 요청을 보냈습니다.");
-    await axios.get(`${RestApi()}/friend/send/${userSequence}/${getter}`);
+    // await axios.get(`${RestApi()}/friend/send`, {
+    await axios.post('http://localhost:8080/api/friend/send', {
+      sendFrd: userSequence,
+      recvFrd: getterSequence
+    });
   };
 
-  // 받은 친구 요청 출력 (이후 axios 요청 만들어지면 바꾸기)
-  const getFriendRequests = [
-    {
-      userSequence: 5,
-      email: "pliot123@naver.com",
-      password: null,
-      nickname: "타타아아",
-      gender: null,
-      telNumber: null,
-      level: null,
-      exp: null,
-      mannerPoint: null,
-      minClearTime: null,
-      totalPlayTime: null,
-      imagePath: null,
-      role: null,
-      enabled: true,
-      username: "pliot123@naver.com",
-      authorities: null,
-      accountNonExpired: true,
-      accountNonLocked: true,
-      credentialsNonExpired: true,
-    },
-  ];
-  // const [getFriendRequests, setGetFriendRequests] = useState([]);
-  // useEffect(() => {
-  //   const fetchData = async () => {
-  //     const result = await axios(`여기엔 친구 요청 목록 조회 api 넣기`);
-  //     setGetFriendRequests(result.data);
-  //   }
-  // }, [userSequence, getFriendRequests])
+  // 받은 친구 요청 출력
+  const [getFriendRequests, setGetFriendRequests] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      await axios(`http://localhost:8080/api/friend/receive?nickName=${nickname}`)
+      .then((res) => {
+        console.log('받은 요청을 출력합니다.', res.data)
+        setGetFriendRequests(res.data);
+      })
+      .then(() => {
+        console.log('wow', getFriendRequests)
+      })
+      .catch(() => {
+        console.log('받은 신청 유저 없음')
+      })
+    }
 
-  // 친구 요청 수락/거절
-  const handleAcceptFriendRequest = async (sender, accept) => {
-    alert(`친구 요청을 ${accept === 1 ? "수락" : "거절"}하셨습니다.`);
-    await axios(
-      `${RestApi()}/friend/accept/${sender}/${userSequence}/${accept}`
-    );
-    // 이후 친구요청목록 다시 request 할 것
+    fetchData();
+  }, [userSequence])
+
+  // 친구 요청 수락, 이후 새로운 친구 목록 출력
+  const handleAcceptFriendRequest = async (sender, getter) => {
+    alert('친구 요청을 수락하셨습니다.')
+    await axios.put(`http://localhost:8080/api/friend/receive/ok`, {
+      sendFrd: sender,
+      recvFrd: getter
+    })
+    .then((res) => {
+      setGetFriendRequests(res.data)
+    });
+  };
+  // 친구 요청 거절
+  const handleCancelFriendRequest = async (sender, getter) => {
+    alert('친구 요청을 거절하셨습니다.')
+    await axios.post(`http://localhost:8080/api/friend/receive/cancel`, {
+      sendFrd: sender,
+      recvFrd: getter
+    })
+    .then((res) => {
+      setGetFriendRequests(res.data)
+    });
   };
 
   return (
@@ -105,39 +111,20 @@ const FriendListModalAdd = (props) => {
       <hr />
 
       {/* 유저가 받은 친구 요청 */}
-      {getFriendRequests ? (
-        <div>
-          <p>친구 요청이 있습니다.</p>
-          {getFriendRequests.map((data, index) => {
+      <div>
+        {Array.isArray(getFriendRequests) && getFriendRequests.map((data, index) => {
             return (
               <div key={index}>
-                <img src={data.profile_img_path} alt="" />
-                Lv.{data.level}
-                {data.nickname}
+                {data.sendNickName}
                 {/* 수락 버튼 */}
-                <button
-                  onClick={() =>
-                    handleAcceptFriendRequest(data.userSequence, 1)
-                  }
-                >
-                  V
-                </button>
+                <button onClick={() => handleAcceptFriendRequest(data.userId, data.frdUserId)}>V</button>
                 {/* 거절 버튼 */}
-                <button
-                  onClick={() =>
-                    handleAcceptFriendRequest(data.userSequence, 0)
-                  }
-                >
-                  X
-                </button>
-                <button></button>
+                <button onClick={() => handleCancelFriendRequest(data.userId, data.frdUserId)}>X</button>
               </div>
             );
-          })}
-        </div>
-      ) : (
-        <p>친구 요청이 없습니다.</p>
-      )}
+          })
+        }
+      </div>
     </div>
   );
 };

4058729bc29f10d8d3bcdceeb9b42bd4de1219a8
JungDongSeop <ds123911@gmail.com>
2023-02-10 02:43:57 +0900

    STYLE: [front] 모달 css 수정

--- frontend/src/components/UI/Modal.js
+++ frontend/src/components/UI/Modal.js
@@ -1,41 +1,45 @@
-import React from 'react';
-import './Modal.css';
+import React, { useState } from 'react';
+import Dialog from '@mui/material/Dialog';
+// import DialogTitle from '@mui/material/DialogTitle';
+import DialogContent from '@mui/material/DialogContent';
+// import DialogActions from '@mui/material/DialogActions';
+import Button from './Button';
 
-/* 출처 https://phrygia.github.io/react/2021-09-21-react-modal/ */
+const Modal = ({ buttonTitle, children, width, height }) => {
+  const [open, setOpen] = useState(false);
 
-const Modal = (props) => {
-  // 열기, 닫기, 모달 헤더 텍스트, footer 존재 여부를 부모로부터 받아옴
-  const { open, close, width, height } = props;
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
 
   return (
-    // 모달이 열릴때 openModal 클래스가 생성된다.
-    <div className={open ? 'openModal modal' : 'modal'}>
-      {open ? (
-        <section
-          style={{
+    <div>
+      <Button variant="outlined" color="primary" onClick={handleClickOpen}>
+        {buttonTitle}
+      </Button>
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        PaperProps={{
+          style: {
             width: width,
             height: height,
-            display: 'flex',
-            flexDirection: 'column',
-            justifyContent: 'space-between',
-          }}
-        >
-          <div>
-            {props.children}
-          </div>
-
-          <button 
-            className="close"
-            onClick={close}
-            style={{
-
-
-            }}
-            >
-            &times;
-          </button>
-        </section>
-      ) : null}
+            margin: 'auto',
+            maxHeight: 'none',
+            maxWidth: 'none',
+            backgroundColor: 'rgba(200, 200, 200, 0.9)',
+            overflow: 'auto'
+          }
+        }}
+      >
+        <DialogContent>
+          {children}
+        </DialogContent>
+      </Dialog>
     </div>
   );
 };
--- frontend/src/components/layout/side_bar/BadgeModal.js
+++ frontend/src/components/layout/side_bar/BadgeModal.js
@@ -1,25 +1,27 @@
-import { useState } from 'react';
+// import { useState } from 'react';
 import Modal from '../../UI/Modal';
-import Button from '../../UI/Button';
 import BadgeImages from '../../../assets/badges/BadgeImages'
 
 const BadgeModal = () => {
 
   // 모달을 열고 닫는 함수
-  const [modalOpen, setModalOpen] = useState(false);
-  const openModal = () => {
-    setModalOpen(true);
-  };
-  const closeModal = () => {
-    setModalOpen(false);
-  };
-
+  // const [modalOpen, setModalOpen] = useState(false);
+  
+  // const openModal = () => {
+  //   setModalOpen(true);
+  // };
+  // const closeModal = () => {
+  //   setModalOpen(false);
+  // };
   // 
   return (
     <div>
       {/* 방 생성 모달 */}
-      <Button onClick={openModal}>뱃지 목록</Button>
-      <Modal open={modalOpen} close={closeModal} height='400px' width='800px'>
+      {/* <Button onClick={openModal}>뱃지 목록</Button> */}
+      {/* <Modal open={modalOpen} close={closeModal} height='400px' width='800px'>
+        <BadgeImages/>
+      </Modal> */}
+      <Modal buttonTitle='뱃지 목록' width='800px' height='800px'>
         <BadgeImages/>
       </Modal>
     </div>

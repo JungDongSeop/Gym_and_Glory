6d9c97deba98732c2826c7552c3cf060dffb6b05
leeseunghan <seunghan4769@naver.com>
2023-02-09 12:07:23 +0900

    ADD: [front] 신고접수

--- backend/src/main/java/com/backend/api/controller/ReportController.java
+++ backend/src/main/java/com/backend/api/controller/ReportController.java
@@ -37,6 +37,7 @@ public class ReportController {
     //신고 권한에 따라서 다른 리스트를 뿌려줌
     @GetMapping("/user/{email}")
     public ResponseEntity<?> getReport(@PathVariable String email){
+        System.out.println(email);
         List<Report> list = reportService.getDivReport(email);
         return new ResponseEntity<>(list,HttpStatus.OK);
     }

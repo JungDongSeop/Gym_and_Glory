ec6102fb57c9f810c3abbe1f2021509615ee6350
leeseunghan <seunghan4769@pukyong.ac.kr>
2023-02-17 00:36:06 +0900

    FIX: [front] 게임 종료 후 경험치 가져옴

--- frontend/src/components/video/UnityGame.js
+++ frontend/src/components/video/UnityGame.js
@@ -175,9 +175,17 @@ const UnityGame = forwardRef((props, ref) => {
           console.log("팀 로그 보내기");
         });
     }
-    axios.post(APPLICATION_SERVER_URL + "game/userlog", myRecord).then(() => {
-      console.log("개인 기록 보내기");
-    });
+    axios
+      .post(APPLICATION_SERVER_URL + "game/userlog", myRecord)
+      .then(() => {
+        console.log("개인 기록 보내기");
+      })
+      .then((res) => {
+        sessionStorage.setItem(
+          "exp",
+          sessionStorage.getItem("exp") + res.myRecord.damage
+        );
+      });
   }, [myNum, myRecord, teamRecord]);
 
   useEffect(() => {
